b56911e6168586bd59825076c2c63f5fda914157
Sergej Garkusha <Tenebrann@googlemail.com>
2021-08-03 20:35:02 +0200

    1.0.1 mini fix

--- src/modules/components/Nav/Nav.tsx
+++ src/modules/components/Nav/Nav.tsx
@@ -4,10 +4,10 @@ import './Nav.scss';
 export default function Nav() {
   return (
     <div className="nav">
-      <a href="#">Main</a>
-      <a href="#">Skills</a>
-      <a href="#">Projects</a>
-      <a href="#">Contacts</a>
+      <a href="/#">Main</a>
+      <a href="/#">Skills</a>
+      <a href="/#">Projects</a>
+      <a href="/#">Contacts</a>
     </div>
   );
 }
