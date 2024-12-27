20cad870691dfbf17ea7ace88faea10eb1fd83a8
Sergej Garkusha <Tenebrann@googlemail.com>
2024-12-27 14:45:13 +0100

    feat: implement form validation with Formik and integrate Material-UI Snackbar for feedback
    - Updated Contacts component to use Formik for form state management and validation.
    - Added Material-UI Snackbar for success and error feedback on form submission.
    - Improved form validation for email, name, and messages with specific error messages.
    - Adjusted button styles to reflect disabled state based on form validation.
    - Refactored code for better readability and reusability.
    - Updated dependencies to include Material-UI components for enhanced UI experience.

--- src/modules/components/Contacts/Contacts.tsx
+++ src/modules/components/Contacts/Contacts.tsx
@@ -1,9 +1,11 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Contacts.scss';
 import { Fade } from 'react-awesome-reveal';
 import { SectionTitle } from '../SectionTitle/SectionTitle';
 import emailjs from 'emailjs-com';
 import { useFormik } from 'formik';
+import Snackbar, { SnackbarCloseReason } from '@mui/material/Snackbar';
+import Alert from '@mui/material/Alert';
 
 export type EmailData = {
   email: string;
@@ -14,6 +16,18 @@ export type EmailData = {
 type FormikErrorType = Partial<EmailData>;
 
 export default function Contacts() {
+  const [value, setValue] = useState<null | string>();
+  const [error, setError] = useState<null | string>();
+
+  const handleClose = (event?: React.SyntheticEvent | Event, reason?: SnackbarCloseReason) => {
+    if (reason === 'clickaway') {
+      return;
+    }
+
+    !!value && setValue(null);
+    !!error && setError(null);
+  };
+
   const formik = useFormik({
     initialValues: {
       email: '',
@@ -39,19 +53,18 @@ export default function Contacts() {
       } else if (values.name.length < 3) {
         errors.name = 'Name must be at least 3 characters long';
       }
-      return errors;
     },
 
     onSubmit: (values, { resetForm }) => {
       emailjs.send('sergej_garkusha', 'template_jfhx7uo', values, '3HQ8iT10tSgZUprx6').then(
         (result) => {
           console.log('Email sent successfully:', result.text);
-          alert('Message sent successfully!');
+          setValue('Message sent successfully!');
           resetForm();
         },
         (error) => {
           console.error('Error sending email:', error.text);
-          alert('Failed to send message, please try again.');
+          setError('Failed to send message, please try again.');
         }
       );
     },
@@ -67,13 +80,25 @@ export default function Contacts() {
       ? false
       : true;
 
-  // #ff5555
   return (
     <div className="contacts">
       <div className="container contacts__container">
         <Fade>
           <SectionTitle title={'CONTACTS'} />
-
+          <Snackbar
+            open={!!value || !!error}
+            autoHideDuration={6000}
+            onClose={handleClose}
+            anchorOrigin={{ vertical: 'top', horizontal: 'right' }}
+            style={{ marginTop: '80px' }}>
+            <Alert
+              onClose={handleClose}
+              severity={error ? 'error' : 'success'}
+              variant="filled"
+              sx={{ width: '100%' }}>
+              {!!value ? value : error}
+            </Alert>
+          </Snackbar>
           <div className="contacts__form__wrapper">
             <form className="contacts__form" onSubmit={formik.handleSubmit}>
               <input
