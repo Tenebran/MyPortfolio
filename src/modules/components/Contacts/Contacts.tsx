76ab9c92fe2036c4eaeee486c624e5e48522d89c
Sergej Garkusha <Tenebrann@googlemail.com>
2024-12-27 13:18:28 +0100

    feat: integrate email sending functionality with EmailJS
    - Added EmailJS integration to the Contacts component.
    - Configured the sendEmail function to handle form submissions.
    - Implemented a simple and user-friendly template for email messages.
    - Added a success and error alert for user feedback after email submission.
    - Enhanced form functionality with reset functionality after successful submission.

--- src/modules/components/Contacts/Contacts.tsx
+++ src/modules/components/Contacts/Contacts.tsx
@@ -2,8 +2,32 @@ import React from 'react';
 import './Contacts.scss';
 import { Fade } from 'react-awesome-reveal';
 import { SectionTitle } from '../SectionTitle/SectionTitle';
+import emailjs from 'emailjs-com';
 
 export default function Contacts() {
+  const sendEmail = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    emailjs
+      .sendForm(
+        'sergej_garkusha',
+        'template_jfhx7uo',
+        e.target as HTMLFormElement,
+        '3HQ8iT10tSgZUprx6'
+      )
+      .then(
+        (result) => {
+          console.log('Email sent successfully:', result.text);
+          alert('Message sent successfully!');
+        },
+        (error) => {
+          console.error('Error sending email:', error.text);
+          alert('Failed to send message, please try again.');
+        }
+      );
+    (e.target as HTMLFormElement).reset();
+  };
+
   return (
     <div className="contacts">
       <div className="container contacts__container">
@@ -11,11 +35,21 @@ export default function Contacts() {
           <SectionTitle title={'CONTACTS'} />
 
           <div className="contacts__form__wrapper">
-            <form className="contacts__form">
-              <input type="name" placeholder="Name" className="contacts__form__name" />
-              <input type="email" placeholder="Email" className="contacts__form__email" />
-              <textarea placeholder="Messages" className="contacts__form__text"></textarea>
-              <button className="contacts__form__button">Send</button>
+            <form className="contacts__form" onSubmit={sendEmail}>
+              <input name="name" type="name" placeholder="Name" className="contacts__form__name" />
+              <input
+                name="email"
+                type="email"
+                placeholder="Email"
+                className="contacts__form__email"
+              />
+              <textarea
+                name="message"
+                placeholder="Messages"
+                className="contacts__form__text"></textarea>
+              <button className="contacts__form__button" type="submit">
+                Send
+              </button>
             </form>
           </div>
         </Fade>
